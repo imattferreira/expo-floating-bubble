0c75109a3bbeddb1f2e0976e2b9d02e240cf7969
Matheus Ferreira <mattferreira.dev@protonmail.com>
2025-07-06 17:02:45 -0300

    cleanup project, also add a code example

--- src/ExpoFloatingBubbleModule.ts
+++ src/ExpoFloatingBubbleModule.ts
@@ -1,12 +1,12 @@
-import { NativeModule, requireNativeModule } from 'expo';
+import { NativeModule, requireNativeModule } from "expo";
 
-import { ExpoFloatingBubbleModuleEvents } from './ExpoFloatingBubble.types';
+import { ExpoFloatingBubbleModuleEvents } from "./ExpoFloatingBubble.types";
 
 declare class ExpoFloatingBubbleModule extends NativeModule<ExpoFloatingBubbleModuleEvents> {
-  PI: number;
-  hello(): string;
-  setValueAsync(value: string): Promise<void>;
+  getTheme: () => string;
 }
 
 // This call loads the native module object from the JSI.
-export default requireNativeModule<ExpoFloatingBubbleModule>('ExpoFloatingBubble');
+export default requireNativeModule<ExpoFloatingBubbleModule>(
+  "ExpoFloatingBubble"
+);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-// Reexport the native module. On web, it will be resolved to ExpoFloatingBubbleModule.web.ts
-// and on native platforms to ExpoFloatingBubbleModule.ts
-export { default } from './ExpoFloatingBubbleModule';
-export { default as ExpoFloatingBubbleView } from './ExpoFloatingBubbleView';
-export * from  './ExpoFloatingBubble.types';
+import ExpoFloatingBubbleModule from "./ExpoFloatingBubbleModule";
+export * from "./ExpoFloatingBubble.types";
+
+export function getTheme(): string {
+  return ExpoFloatingBubbleModule.getTheme();
+}
